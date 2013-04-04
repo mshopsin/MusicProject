c41b830fd1a1216acc00368e75321074d276aae9
Brian&Ian <david+luke@appacademy.io>
2013-04-04 09:41:52 -0700

    Them tables be done

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -1,4 +1,5 @@
 class Recording < ActiveRecord::Base
   belongs_to :band
+  belongs_to :single
   attr_accessible :name, :yr
 end
--- db/migrate/20130404163220_create_recordings.rb
+++ db/migrate/20130404163220_create_recordings.rb
@@ -3,10 +3,11 @@ class CreateRecordings < ActiveRecord::Migration
     create_table :recordings do |t|
       t.string :name
       t.references :band
+      t.references :single
       t.integer :yr
 
       t.timestamps
     end
-    add_index :recordings, :band_id
+    add_index :recordings, :band_id, :single_id
   end
 end
